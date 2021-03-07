3a684ab8e3ff71a053760625fa8c861b15aaa44a
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-07 16:43:18 +0100

    feat: implement function to check brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let leftBrackets = [];
+    let rightBrackets = [];
+    let strBrackets = {}
+    for (const bracketsConfigElement of bracketsConfig) {
+        leftBrackets.push(bracketsConfigElement[0]);
+        rightBrackets.push(bracketsConfigElement[1]);
+
+        strBrackets[bracketsConfigElement[0]] = bracketsConfigElement[1];
+    }
+
+    let strLeftBrackets = [];
+    for (let i = 0; i < str.length; i++) {
+        if (leftBrackets.includes(str[i]) && (!rightBrackets.includes(str[i]) || !strLeftBrackets.includes(str[i]))) {
+            strLeftBrackets.push(str[i]);
+        } else if (rightBrackets.includes(str[i])) {
+            if (strBrackets[strLeftBrackets.pop()] !== str[i]) {
+                return false;
+            }
+        }
+    }
+    return !strLeftBrackets.length;
 }
